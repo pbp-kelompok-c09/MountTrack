ea0e3de768202eb9b60a69879f21772681d2ffb3
Farrel Arrayyan <farrel0810@gmail.com>
2025-10-10 17:41:04 +0700

    add default path (login page)

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "userprofile"
 
 urlpatterns = [
+    path("", views.login_user, name="default"),
     path("register/", views.register_user, name="register"),
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
