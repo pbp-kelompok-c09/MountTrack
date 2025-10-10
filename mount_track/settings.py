e73bd8c1933e1607db1b3dc78da33e213d28330a
Farrel Arrayyan <farrel0810@gmail.com>
2025-10-10 17:41:39 +0700

    add BASE_DIR/templates to DIRS

--- mount_track/settings.py
+++ mount_track/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'mount_track.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
