b39e4747eb8b722f371f6a6d0c3559bdf29798d3
Farrel Arrayyan <farrel0810@gmail.com>
2025-10-22 16:11:16 +0700

    fix url routing

--- mount_track/urls.py
+++ mount_track/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('userprofile.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # sistem login/logout Django
+    path('', include('news.urls')),
+    path('accounts/', include('userprofile.urls')),
     path('community/', include('community.urls')), 
 ]
 
