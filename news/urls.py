f50418d9f0665ec1e0aed21541ff69a86b554979
ForgerPhysics <sihalohoryan2@gmail.com>
2025-12-01 13:00:31 +0700

    news searchbar like

--- news/urls.py
+++ news/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('', views.show_main, name='page_news'),
     path('news/<uuid:news_id>/', views.show_news, name='show_news'),
     path('create_news', views.create_news, name='create_news'),
-    path('news/delete/<uuid:news_id>/', views.delete_news, name='delete_news'),
-    path('news/edit/<uuid:news_id>/', views.edit_news, name='edit_news'),
+    path('delete/<uuid:news_id>/', views.delete_news, name='delete_news'),
+    path('edit/<uuid:news_id>/', views.edit_news, name='edit_news'),
     path('search/', views.search_news, name='search_news'),
     path('like/<uuid:news_id>/', views.like_news, name='like_news'),
+    path('json/', views.show_json, name='show_json'),
     
     
 ]
