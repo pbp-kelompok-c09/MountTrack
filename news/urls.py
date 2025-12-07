db383d960630e3fa3e1ca34ff6155249496ae20b
ForgerPhysics <sihalohoryan2@gmail.com>
2025-12-07 17:59:08 +0700

    news ui ux, config

--- news/urls.py
+++ news/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('create-flutter/', views.create_news_flutter, name='create_news_flutter'),
     path('user-status/', views.get_user_status, name='get_user_status'),
     path('edit-flutter/<uuid:news_id>/', views.edit_news_flutter, name='edit_news_flutter'),
+    path('increment-view/<uuid:news_id>/', views.increment_view_flutter, name='increment_view_flutter'),
+
     
     
 ]
